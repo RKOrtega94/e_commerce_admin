0162bfc229051ddf02a6947775a5aa636e5b1fc2
RKOrtega94 <rkortega1994@gmail.com>
2023-11-30 16:22:20 -0500

    Fix bug in login functionality

---
+++ lib/src/presentation/view_controller/modal_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomModal {
+  static void show({
+    required String title,
+    required String message,
+    required String buttonText,
+    required Function() onPressed,
+  }) =>
+      Get.bottomSheet(
+        Container(
+          color: Colors.white,
+          child: Wrap(
+            children: [
+              ListTile(
+                title: Text(title),
+                subtitle: Text(message),
+              ),
+              const Divider(),
+              ListTile(
+                title: Text(buttonText),
+                onTap: onPressed,
+              ),
+            ],
+          ),
+        ),
+        isScrollControlled: true,
+      );
+}
