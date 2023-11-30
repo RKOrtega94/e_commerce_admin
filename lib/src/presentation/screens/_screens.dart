acdf88692711006be7a4cd4348a72de8acf66f94
RKOrtega94 <rkortega1994@gmail.com>
2023-11-30 14:34:46 -0500

    Add new screens

--- lib/src/presentation/screens/_screens.dart
+++ lib/src/presentation/screens/_screens.dart
@@ -1,6 +1,12 @@
+// ====== Home ======
 export 'home/home_screen.dart';
+// ====== End Home ======
 
+// ====== Product ======
 export 'product/product_screen.dart';
+// ====== End Product ======
 
+// ====== Category ======
 export 'category/category_screen.dart';
 export 'category/category_form.dart';
+// ====== End Category ======
