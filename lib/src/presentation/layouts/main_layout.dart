63628319b56c75042cafab82f5d6c0f0a0fa5358
RKOrtega94 <rkortega1994@gmail.com>
2023-11-30 15:16:32 -0500

    Add showAppBar property to AppMainLayout

--- lib/src/presentation/layouts/main_layout.dart
+++ lib/src/presentation/layouts/main_layout.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 class AppMainLayout extends StatelessWidget {
   final Widget child;
+  final bool showAppBar;
   const AppMainLayout({
     super.key,
     required this.child,
+    this.showAppBar = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: showAppBar
+          ? AppBar(
+              title: const Text('Admin E-Commerce'),
+            )
+          : null,
       body: child,
     );
   }
--- lib/src/presentation/screens/product/product_form_screen.dart
+++ lib/src/presentation/screens/product/product_form_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_admin/src/presentation/layouts/main_layout.dart';
 import 'package:flutter/material.dart';
 
 /// [ProductFormScreen] displays the product form screen.
@@ -8,6 +9,10 @@ class ProductFormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const AppMainLayout(
+      child: Center(
+        child: Text('Product Form Screen'),
+      ),
+    );
   }
 }
