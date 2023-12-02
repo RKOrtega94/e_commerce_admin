bd326a81c4de10a3569449da139c7a007bbde70c
Robinson Ortega <rkortega1994@gmail.com>
2023-12-02 17:50:45 +0100

    Add Firebase initialization and environment variable support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:e_commerce_admin/firebase_options.dart';
 import 'package:e_commerce_admin/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  // Secure initialization
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
